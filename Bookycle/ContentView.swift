ff266e1765cc4514eecb2a98449aaf449b3dfffb
Giovanni Fioretto <giovanni.fioretto.23@gmail.com>
2024-12-09 12:20:49 +0100

    add git ignore

--- Bookycle/ContentView.swift
+++ Bookycle/ContentView.swift
@@ -9,13 +9,13 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        TabView(selection: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Selection@*/.constant(1)/*@END_MENU_TOKEN@*/) {
+            Text("Tab Content 1").tabItem { /*@START_MENU_TOKEN@*/Text("Tab Label 1")/*@END_MENU_TOKEN@*/ }.tag(1)
+            Image(systemName: "plus").tabItem { /*@START_MENU_TOKEN@*/Text("Tab Label 2")/*@END_MENU_TOKEN@*/ }.tag(2)
+            
         }
-        .padding()
+        
+        
     }
 }
 
