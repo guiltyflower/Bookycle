b505b2f68e7d9b80473081b91c9ee6ccd5f1050b
Giovanni Fioretto <giovanni.fioretto.23@gmail.com>
2024-12-09 11:12:36 +0100

    Initial Commit

---
+++ Bookycle/BookycleApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookycleApp.swift
+//  Bookycle
+//
+//  Created by Giovanni Fioretto on 09/12/24.
+//
+
+import SwiftUI
+
+@main
+struct BookycleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
